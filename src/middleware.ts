478871c7e88b8fbb98c9ae623ecc40fb4f272eed
Manu09 <manabda751@gmail.com>
2025-02-16 20:42:58 +0530

    Updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,25 @@
-import { getToken } from 'next-auth/jwt';
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest) {
-  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  // If the user is authenticated, redirect them away from /api/auth/signin
-  if (session && req.nextUrl.pathname.startsWith('/api/auth/signin')) {
-    return NextResponse.redirect(new URL('/', req.url)); // Redirect to homepage or any other page
-  }
+    const path = req.nextUrl.pathname;
+    const publicPath = ['/api/auth/signin', ];
+    const isPublicPath = publicPath.includes(path);
 
-  return NextResponse.next();
+    const sessionToken = req.cookies.get('next-auth.session-token')?.value || req.cookies.get('__Secure-next-auth.session-token')?.value;
+
+    if (isPublicPath && sessionToken) {
+        return NextResponse.redirect(new URL('/', req.url));
+    }
+    if (!isPublicPath && !sessionToken) {
+        return NextResponse.redirect(new URL('/api/auth/signin', req.url));
+    }
+
+    return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/api/auth/signin'],
+    matcher: [
+        '/api/auth/signin'
+    ],
 };
